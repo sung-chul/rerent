8495145ffec83f5d54492f5e5c0bf0907b70aa25
minho5814 <tkmino84@gmail.com>
2017-06-13 16:05:55 +0900

    라디오 버튼 수정

--- js/common.js
+++ js/common.js
@@ -247,6 +247,8 @@ $(window).load(function(){
 	$('input[type=radio].styled1').each(function(){
 		$(this).parents('label').addClass('check-radio');
 		$(this).wrap('<span class="radiobox">');
+	});
+	$('input[type=radio].styled1').change(function(){
 		if(this.checked){
 			$(this).parents('.radiobox').addClass('checked');
 		}
